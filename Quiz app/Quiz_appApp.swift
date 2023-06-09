0074837def97e6181bc7ef9fcbdb7e568a2be893
Leah N <leah.niebuhr@gmail.com>
2023-06-09 10:28:37 -0500

    Initial Commit

---
+++ Quiz app/Quiz_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_appApp.swift
+//  Quiz app
+//
+//  Created by Scholar on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
